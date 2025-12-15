50f222e7833920783b3ea6ba52673c370709c367
Sergio Romero <hask@MacBook-Air-de-Sergio.local>
2025-12-15 11:51:46 +0100

    Subida grupos

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/autoescuela_groups.xml'
         'views/views.xml',
         'views/templates.xml',
     ],

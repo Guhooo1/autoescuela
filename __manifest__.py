ed459a764c945c8694692349e0871faaceb87581
Guhooo1 <hujo2581@gmail.com>
2026-01-13 09:25:11 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,41 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "autoescuela",
-
-    'summary': "Gestion de autoescuelas",
-
-    'description': """
-        Modulo para la gestion de autoescuelas 
-    """,
-
+    'name': "Autoescuela",
+    'summary': "Gestión de autoescuelas",
+    'description': "Módulo para la gestión de autoescuelas",
     'author': "Sergio, Juan y Hugo",
-    'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'education',
-    'version': '0.1',
-    'application': 'True'
-
-    # any module necessary for this one to work correctly
+    'category': 'Education',
+    'version': '1.0',
     'depends': ['base'],
+    'application': True,
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'security/autoescuela_groups.xml',
-        'views/views.xml',
-        'views/autoescuela_alumno_view.xml',
+
         'views/autoescuela_autoescuela_view.xml',
+        'views/autoescuela_profesor_view.xml',
+        'views/autoescuela_alumno_view.xml',
         'views/autoescuela_examen_view.xml',
         'views/autoescuela_menus.xml',
-        'views/autoescuela_profesor_view.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
 

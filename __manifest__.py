68a50f9382159068de18902defa225b9637e2d77
Sergio Romero <153959138+SergioRomeroOfficial@users.noreply.github.com>
2026-01-13 08:41:09 +0100

    Cambios manifest 2

--- __manifest__.py
+++ __manifest__.py
@@ -16,6 +16,7 @@
     # for the full list
     'category': 'education',
     'version': '0.1',
+    'application': 'True'
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
